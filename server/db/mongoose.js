55a98965cd170993c9d13a3bc72574ca739aa8e6
alvinlu7 <alvinlu7@gmail.com>
2018-06-29 17:14:54 -0700

    Fixing mongodb uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku_wpx47wn7:s0gudm3et64f17svp837l1n0uk@ds223161.mlab.com:23161/heroku_wpx47wn7'||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-module.exports = {
-    mongoose
-};
+module.exports = {mongoose};
